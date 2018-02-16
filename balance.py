b3e31d973fe02ae7142a5d86cedfb3f6dcfe10a4
smrithi94 <33006213+smrithi94@users.noreply.github.com>
2018-02-15 20:55:41 -0800

    Add files via upload

---
+++ balance.py
@@ -0,0 +1,4 @@
+costOfItem=raw_input('enter the cost of the item')
+billsGiven=raw_input ('enter the cost of second item')
+balance=float(billsGiven)-float(costOfItem)
+print 'your item costs',costOfItem,'you gave',billsGiven,'and your balance is',balance
