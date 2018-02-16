d3b6321b3d67f285d945d9a8b06293460f702661
smrithi94 <33006213+smrithi94@users.noreply.github.com>
2018-02-15 20:56:53 -0800

    Add files via upload

---
+++ fullname.py
@@ -0,0 +1,5 @@
+firstName=raw_input('enter your first name')
+lastName=raw_input('enter your last name')
+fullName=firstName+lastName
+print fullName
+print 'hello',firstName,' ',lastName,'welcome to python programming'
