fd7cfe79ed84a37b7a0baaa56d2c2aad59cf991c
smrithi94 <33006213+smrithi94@users.noreply.github.com>
2018-02-15 20:51:22 -0800

    Add files via upload

---
+++ addresult.py
@@ -0,0 +1,6 @@
+numOne=raw_input('get the first number')
+numOne=int(numOne)
+numTwo=raw_input('get the second number')
+numTwo=int(numTwo)
+result=numOne+numTwo
+print 'The sum of',numOne, 'and',numTwo, 'is' ,result
