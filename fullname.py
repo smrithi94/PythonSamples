2654605289d915577fb9f483769e283a1305c573
smrithi94 <33006213+smrithi94@users.noreply.github.com>
2018-10-05 16:22:55 -0700

    Delete fullname.py

--- fullname.py
+++
@@ -1,5 +0,0 @@
-firstName=raw_input('enter your first name')
-lastName=raw_input('enter your last name')
-fullName=firstName+lastName
-print fullName
-print 'hello',firstName,' ',lastName,'welcome to python programming'
